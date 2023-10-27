bfb68f44e01cb3c671be5d7daa0f0921b638e14c
N4rdelli <nardellianelize20@gmail.com>
2023-10-27 17:04:41 -0300

    Fim do dia

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -2,6 +2,11 @@ import styles from "./Home.module.css";
 import imgDogs from "../../assets/dogsImage.jpg";
 import Box from "../../components/box/box";
 
+import dogs from "../../assets/dogs.jpg";
+import cats from "../../assets/dogs.jpg";
+import birds from "../../assets/dogs.jpg";
+import others from "../../assets/dogs.jpg";
+
 const Home = () => {
   return (
     <>
@@ -18,6 +23,17 @@ const Home = () => {
             <Box icon={"https://placehold.co/30x30"} text={"Banho e Tosa"}/>
             <Box icon={"https://placehold.co/30x30"} text={"Vacinação"}/>
             <Box icon={"https://placehold.co/30x30"} text={"Treinamento"}/>
+          </div>
+        </div>
+        <div className={styles.produtos}>
+          <div className={styles.texto}>
+            <p>Texto Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+          </div>
+          <div className={styles.animais}>
+            <div> <img src={dogs} alt="Cachorros" width={600} height={250} /> </div>
+            <div> <img src="https://placehold.co/275x250" alt="Gatos" width={275} height={250} /> </div>
+            <div> <img src="https://placehold.co/275x250" alt="Pássaros" width={275} height={250} /> </div>
+            <div> <img src="https://placehold.co/600x250" alt="Roedores" width={600} height={250} /> </div>
           </div>
         </div>
         
